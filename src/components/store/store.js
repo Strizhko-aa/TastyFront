1737b8e18b1bd505f7c8dfc5ee5d9563b648909d
Andre <amm-andreistrizhko@yandex.ru>
2019-04-17 21:33:15 +0300

    правил в слепую(бек временно мертв), надеюсь, все работает как надо (:

--- src/components/store/store.js
+++ src/components/store/store.js
@@ -6,7 +6,8 @@ Vue.use(Vuex)
 // это vueX - хранилище чтобы перебрасывать значения между любыми компонентами прокта
 const store = new Vuex.Store({
   state: {
-    whereIsUser: '' //  выводми сверху где юзер. Меняется в main.js при переходе
+    whereIsUser: '', //  выводми сверху где юзер. Меняется в main.js при переходе
+    selectedInWaiterMenu: 'tables'
   },
   mutations: {
     setValue (store, keyValue) {
