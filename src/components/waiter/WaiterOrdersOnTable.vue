33a1d9a0fab3be5c3f75f4830e0b81635161aa42
Andre <amm-andreistrizhko@yandex.ru>
2019-05-19 23:04:27 +0300

    add text on empty aleret in WaiterOrdersOnTable.vue

--- src/components/waiter/WaiterOrdersOnTable.vue
+++ src/components/waiter/WaiterOrdersOnTable.vue
@@ -11,53 +11,56 @@
   <b-container v-else fluid>
     <p hidden>{{refreshWaiter}}</p>
     <div v-show="false">{{tableNumber}}</div>
-    <b-container style="text-align: left" v-for="item in parsedOrders" :key="item.id">
-      <b-row>
-        <!-- <b-col offset="0" offset-sm="3" offset-md="3" offset-lg="3"></b-col> -->
-        <b-col cols sm md lg = "6">
-          <p class="order-number">
-            Заказ №{{item.id}} | {{item.total}} &#8381;
-            <i v-if="item.typePayment.id === 1" class="fas fa-credit-card"></i>
-            <i v-if="item.typePayment.id === 2" class="fas fa-money-bill-wave"></i>
-          </p>
-        </b-col>
-      </b-row>
-      <i v-if="getCount(1, item) > 0" class="fas fa-clock status"> В ожидании</i>
-      <b-row class="dishes-in-order" v-for="(dish, index) in item.parsedDishes" v-show="dish.status.id === 1" :key="'wait' + index">
-        <b-col offset=1 offset-sm=1 offset-md=1 offset-lg=1></b-col>
-        <b-col cols=11 sm=11 md=11 lg=11>
-          <span class="dish-name">{{dish.dish.name}} x {{dish.count}}</span>
-        </b-col>
-        <!-- <b-col cols=12 sm=4 md=4 lg=4> </b-col> -->
-        <b-col cols=11 sm=11 md=11 lg=11></b-col>
-      </b-row>
-      <i v-if="getCount(2, item) > 0" class="fas fa-fire status"> Готовится</i>
-      <b-row class="dishes-in-order" v-for="(dish, index) in item.parsedDishes" v-show="dish.status.id === 2" :key="'inProgerss' + index">
-        <b-col offset=1 offset-sm=1 offset-md=1 offset-lg=1></b-col>
-        <b-col cols=11 sm=11 md=11 lg=11>
-          <span class="dish-name">{{dish.dish.name}} x {{dish.count}}</span>
-        </b-col>
-      </b-row>
-      <i v-if="getCount(3, item) > 0" class="fas fa-check status"> Готово</i>
-      <b-row class="dishes-in-order" v-for="(dish, index) in item.parsedDishes" v-show="dish.status.id === 3" :key="'ready' + index">
-        <b-col offset=1 offset-sm=1 offset-md=1 offset-lg=1></b-col>
-        <b-col cols=11 sm=11 md=11 lg=11>
-          <span class="dish-name">{{dish.dish.name}} x {{dish.count}}</span>
-          <img class="status-icon" @click="deliveryDish(dish)" src="../../assets/images/arrow-right.svg" alt="">
-        </b-col>
-        <!-- <b-col cols=12 sm=4 md=4 lg=4>  </b-col> -->
-        <!-- <b-col cols=1 sm=1 md=1 lg=1 ></b-col> -->
-      </b-row>
-      <i v-if="getCount(4, item) > 0" class="fas fa-check-double status"> Отнесено</i>
-      <b-row class="dishes-in-order" v-for="(dish, index) in item.parsedDishes" v-show="dish.status.id === 4" :key="'delivered' + index">
-        <b-col offset=1 offset-sm=1 offset-md=1 offset-lg=1></b-col>
-        <b-col cols=11 sm=11 md=11 lg=11>
-          <span class="dish-name">{{dish.dish.name}} x {{dish.count}}</span>
-        </b-col>
-        <!-- <b-col cols=12 sm=4 md=4 lg=4>  </b-col> -->
-        <!-- <b-col cols=1 sm=1 md=1 lg=1 @click="deliveryDish(dish)"><img class="status-icon" src="../../assets/images/arrow-right.svg" alt=""></b-col> -->
-      </b-row>
-    </b-container>
+    <div v-if="parsedOrders.length > 0">
+      <b-container style="text-align: left" v-for="item in parsedOrders" :key="item.id">
+        <b-row>
+          <!-- <b-col offset="0" offset-sm="3" offset-md="3" offset-lg="3"></b-col> -->
+          <b-col cols sm md lg = "6">
+            <p class="order-number">
+              Заказ №{{item.id}} | {{item.total}} &#8381;
+              <i v-if="item.typePayment.id === 1" class="fas fa-credit-card"></i>
+              <i v-if="item.typePayment.id === 2" class="fas fa-money-bill-wave"></i>
+            </p>
+          </b-col>
+        </b-row>
+        <i v-if="getCount(1, item) > 0" class="fas fa-clock status"> В ожидании</i>
+        <b-row class="dishes-in-order" v-for="(dish, index) in item.parsedDishes" v-show="dish.status.id === 1" :key="'wait' + index">
+          <b-col offset=1 offset-sm=1 offset-md=1 offset-lg=1></b-col>
+          <b-col cols=11 sm=11 md=11 lg=11>
+            <span class="dish-name">{{dish.dish.name}} x {{dish.count}}</span>
+          </b-col>
+          <!-- <b-col cols=12 sm=4 md=4 lg=4> </b-col> -->
+          <b-col cols=11 sm=11 md=11 lg=11></b-col>
+        </b-row>
+        <i v-if="getCount(2, item) > 0" class="fas fa-fire status"> Готовится</i>
+        <b-row class="dishes-in-order" v-for="(dish, index) in item.parsedDishes" v-show="dish.status.id === 2" :key="'inProgerss' + index">
+          <b-col offset=1 offset-sm=1 offset-md=1 offset-lg=1></b-col>
+          <b-col cols=11 sm=11 md=11 lg=11>
+            <span class="dish-name">{{dish.dish.name}} x {{dish.count}}</span>
+          </b-col>
+        </b-row>
+        <i v-if="getCount(3, item) > 0" class="fas fa-check status"> Готово</i>
+        <b-row class="dishes-in-order" v-for="(dish, index) in item.parsedDishes" v-show="dish.status.id === 3" :key="'ready' + index">
+          <b-col offset=1 offset-sm=1 offset-md=1 offset-lg=1></b-col>
+          <b-col cols=11 sm=11 md=11 lg=11>
+            <span class="dish-name">{{dish.dish.name}} x {{dish.count}}</span>
+            <img class="status-icon" @click="deliveryDish(dish)" src="../../assets/images/arrow-right.svg" alt="">
+          </b-col>
+          <!-- <b-col cols=12 sm=4 md=4 lg=4>  </b-col> -->
+          <!-- <b-col cols=1 sm=1 md=1 lg=1 ></b-col> -->
+        </b-row>
+        <i v-if="getCount(4, item) > 0" class="fas fa-check-double status"> Отнесено</i>
+        <b-row class="dishes-in-order" v-for="(dish, index) in item.parsedDishes" v-show="dish.status.id === 4" :key="'delivered' + index">
+          <b-col offset=1 offset-sm=1 offset-md=1 offset-lg=1></b-col>
+          <b-col cols=11 sm=11 md=11 lg=11>
+            <span class="dish-name">{{dish.dish.name}} x {{dish.count}}</span>
+          </b-col>
+          <!-- <b-col cols=12 sm=4 md=4 lg=4>  </b-col> -->
+          <!-- <b-col cols=1 sm=1 md=1 lg=1 @click="deliveryDish(dish)"><img class="status-icon" src="../../assets/images/arrow-right.svg" alt=""></b-col> -->
+        </b-row>
+      </b-container>
+    </div>
+    <div v-else><h1>Нет заказов</h1></div>
   </b-container>
 </div>
 </template>
